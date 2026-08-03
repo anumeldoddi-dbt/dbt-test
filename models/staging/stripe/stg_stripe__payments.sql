98fec18504801b59f0d0a76ece912b09eda19892
anuradha.meldoddi@relevancelab.com <anuradha.meldoddi@relevancelab.com>
2026-08-03 06:04:38 +0000

    Commiting all cahnges to Remote branch

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,6 +1,6 @@
 select 
-    orderid,
-    paymentmethod,
-    status,
-    amount
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status as payment_status,
+    amount as payment_amount
     from raw.stripe.payment
